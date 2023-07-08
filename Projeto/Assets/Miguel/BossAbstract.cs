24b140d08f610b3952c555cc20925af89d4a7d9d
Migalhaz <105467709+Migalhaz@users.noreply.github.com>
2023-07-08 10:56:32 -0300

    boss e ray terminados

--- Projeto/Assets/Miguel/BossAbstract.cs
+++ Projeto/Assets/Miguel/BossAbstract.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class BossAbstract : MonoBehaviour
 {
+    protected bool m_canTakeDamage;
     public virtual void Damage()
     {
+        if (!m_canTakeDamage) return;
         Destroy(gameObject);
     }
 }
--- Projeto/Assets/Miguel/PiraScript.cs
+++ Projeto/Assets/Miguel/PiraScript.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class PiraScript : BossAbstract
 {
+    [Header("Attack Settings")]
     [SerializeField] List<GameObject> m_attackRay;
+    bool m_isAttacking;
+    [SerializeField, Min(0)] float m_attackDelay;
+
+    [Header("Rotate Settings")]
     [SerializeField] Vector2 m_rangeTimer;
     [SerializeField, Min(0)] float m_rotateSpeed;
-    [SerializeField, Min(0)] float m_attackDelay;
     float m_currentSpeed;
     float m_currentTimer;
     Vector3 m_eulerAngles;
-    bool canTakeDamage;
-    bool m_isAttacking;
 
     private void Awake()
     {
         SetCurrentTimer();
+        SetRayActive(false);
     }
 
     private void Update()
@@ -31,7 +34,6 @@ public class PiraScript : BossAbstract
         {
             Attack();
         }
-        
     }
 
     void Rotate()
@@ -43,26 +45,33 @@ public class PiraScript : BossAbstract
 
     void Attack()
     {
-        if (m_isAttacking) return; 
-        m_currentSpeed = 0f;
+        if (m_isAttacking) return;
         StartCoroutine(nameof(AttackEffect));
     }
 
     IEnumerator AttackEffect()
     {
         m_isAttacking = true;
-        canTakeDamage = true;
-        yield return new WaitForSeconds(m_attackDelay);
-        Debug.Log("Plou");
+        m_canTakeDamage = true;
+        yield return new WaitForSeconds(m_attackDelay * 0.5f);
+        SetRayActive(true);
+        yield return new WaitForSeconds(m_attackDelay * 0.5f);
+        SetRayActive(false);
         m_isAttacking = false;
-        canTakeDamage = false;
+        m_canTakeDamage = false;
         SetCurrentTimer();
+    }
+
+    void SetRayActive(bool active)
+    {
+        foreach (GameObject ray in m_attackRay)
+        {
+            ray.SetActive(active);
+        }
     }
 
     void SetCurrentTimer()
     {
         m_currentTimer = Random.Range(m_rangeTimer.x, m_rangeTimer.y);
     }
-
-
 }
