db8108c2d015e87a83285ef8cf0813d7774adc02
juliano-CN <juliano.nagy@hotmail.com>
2023-07-09 13:09:48 -0300

    BackupCommit

--- Projeto/Assets/Juliano/TeletransportScript.cs
+++ Projeto/Assets/Juliano/TeletransportScript.cs
@@ -8,9 +8,14 @@ public class TeletransportScript : MonoBehaviour
     [SerializeField] public static bool lance_field = false;
 
     public PlayerCollider pc;
+    CameraController cameraController;
     // Start is called before the first frame update
 
     // Update is called once per frame
+    private void Start()
+    {
+        cameraController = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraController>();
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.B))
@@ -20,7 +25,7 @@ public class TeletransportScript : MonoBehaviour
                 if (!pc.canTeleportForever && !pc.canTeleport) return;
                 transform.position = new Vector3(lance.transform.position.x,
                     lance.transform.position.y,lance.transform.position.z);
-
+                cameraController.ForceCamPosition();
                 //Destroy(lance);
                 lance.SetActive(false);
                 lance_field = false;
